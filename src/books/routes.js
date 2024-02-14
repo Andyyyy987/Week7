c4ce7158c48208ee615bc4d3346aa4aa5cb49385
Andyyyy987 <am38387@gmail.com>
2024-02-14 09:14:19 +0000

    controllers/routes tweaks

--- src/books/routes.js
+++ src/books/routes.js
@@ -1,3 +1,4 @@
+// Get Router from express
 const {Router} = require("express");
 const bookRouter = Router();
 
@@ -7,5 +8,6 @@ const { addBook } = require("./controllers");
 
 // Add a book to the database
 bookRouter.post("/books/addBook", addBook);
+bookRouter.get("/books/getallbooks");
 
 module.exports = bookRouter;

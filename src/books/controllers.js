0dcc053f7c3c9e986a67272adf6152ca632cc458
Andyyyy987 <am38387@gmail.com>
2024-02-16 08:35:43 +0000

    controllers/routes/server modifications

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -1,9 +1,9 @@
 // importing Book from the model file
 const Book = require("./model");
 
+// Add one book to the database
 const addBook = async (request, response) => {
-    try {
-        // Add one book to the database
+    try {        
         const book = await Book.create({    
             title: request.body.title,
             author: request.body.author,
@@ -14,6 +14,32 @@ const addBook = async (request, response) => {
         response.send({ message: "error", error:error});       
     }
 };
+
+const logTypeOfResult = async (result) => {
+    console.log(`Typeof result: ${typeof result} - result: ${result}`);
+  };
+
+// Get all books from the database
+app.get("/books/getAllBooks", async (request, response) => {
+    try {
+    // Send a request to the database with a response back
+    const books = await Book.find({}); 
+    await logTypeOfResult(books);
+    response.send({message: "Success for getting every book", books:books});     
+    } catch (error) {
+        response.send({ message: "error", error:error});
+    }
+});
+
+// Delete a book from the database
+app.delete("/books", async(request, response) => {
+    try {
+    const book = await Book.deleteOne ({ title: request.body.title});
+    response.send({ message: "Deleted", book: book });
+    } catch (error) {
+        response.send({ message: "error", error:error});
+    }
+});
 
 module.exports = {
     // key: function name
--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 // load dotenv for files to use
 require("dotenv").config();
-// making express node module available
+// making express node module and mongoose database available
 const express = require("express");
 const mongoose = require("mongoose");
 
 const connection = require("./db/connection");
-
 const bookRouter = require("./books/routes");
 
 // making express node module data available e.g. app.listen
@@ -13,34 +12,10 @@ const app = express();
 
 app.use(express.json());
 
+// connect to MongoDB
 connection();
 
 app.use(bookRouter);
-
-const logTypeOfResult = async (result) => {
-  console.log(`Typeof result: ${typeof result} - result: ${result}`);
-};
-
-// Get all books from the database
-app.get("/books/getAllBooks", async (request, response) => {
-  // Send a request to the database with a response back
-  const books = await Book.find({}); 
-  await logTypeOfResult(books);
-  response.send({message: "Success for getting every book", books:books}); 
-});
-
-// https://mongoosejs.com/docs/api/model.html#Model.findOneAndUpdate()
-//              Or !!!!!!!!!!!!!!!!!!!!!
-// https://mongoosejs.com/docs/api/model.html#Model.updateOne()
-app.put("/books", (request, reponse) => {
-  // update a single book's author by title
-});
-
-// Delete a book from the database
-app.delete("/books", async(request, response) => {
-  const book = await Book.deleteOne ({ title: request.body.title});
-  response.send({ message: "Deleted", book: book });
-});
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
